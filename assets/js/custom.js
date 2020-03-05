51ed5176cf621e0a52f63bd82144eae30ef8afcc
Yury Kirilyuk <yury@cyberiansoft.com>
2020-03-06 00:05:58 +0200

    320рх

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,6 +36,12 @@ jQuery(function($){
 		return false;
 	});
 
+    $('.cta').click(function(){
+        $('.btn-toggle').toggleClass('active');
+        $('.menu-container').toggleClass('show');
+        return false;
+    });
+
     jQuery(document).click(function(event) {
         if (!jQuery(event.target).closest('.menu-container').length) {
             jQuery('.menu-container.show').removeClass('show');
@@ -43,59 +49,5 @@ jQuery(function($){
         }
     });
 
-
-
-	
-	/* components */
-	
-	/*
-	
-	if($('.styled').length) {
-		$('.styled').styler();
-	};
-	if($('.fancybox').length) {
-		$('.fancybox').fancybox({
-			margin  : 10,
-			padding  : 10
-		});
-	};
-	if($('.slick-slider').length) {
-		$('.slick-slider').slick({
-			dots: true,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			responsive: [
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: "unslick"
-				}				
-			]
-		});
-	};
-	if($('.scroll').length) {
-		$(".scroll").mCustomScrollbar({
-			axis:"x",
-			theme:"dark-thin",
-			autoExpandScrollbar:true,
-			advanced:{autoExpandHorizontalScroll:true}
-		});
-	};
-	
-	*/
-	
-	/* components */
-	
-	
 
 });
